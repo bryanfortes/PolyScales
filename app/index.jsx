dd4c054086b370ec7d233911a1961c95be3ebb91
bryanfortes <bryanfortes1@gmail.com>
2023-06-10 16:51:07 -0400

    Initial commit w/ react

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+/* 
+state
+lifecycle
+ui*/
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+const rootElement = document.getElementById("app");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
